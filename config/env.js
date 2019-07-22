7805d171657b88ba02969c2566740b293b4c64ad
朱双杰 <zhushuangjie@learnta.com>
2019-07-22 17:34:26 +0800

    test save format

--- config/env.js
+++ config/env.js
@@ -1,17 +1,17 @@
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const path = require('path')
-const paths = require('./paths')
+const fs = require("fs");
+const path = require("path");
+const paths = require("./paths");
 
 // Make sure that including paths.js after env.js will read .env variables.
-delete require.cache[require.resolve('./paths')]
+delete require.cache[require.resolve("./paths")];
 
-const NODE_ENV = process.env.NODE_ENV
+const NODE_ENV = process.env.NODE_ENV;
 if (!NODE_ENV) {
   throw new Error(
-    'The NODE_ENV environment variable is required but was not specified.'
-  )
+    "The NODE_ENV environment variable is required but was not specified."
+  );
 }
 
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
@@ -21,9 +21,9 @@ var dotenvFiles = [
   // Don't include `.env.local` for `test` environment
   // since normally you expect tests to produce the same
   // results for everyone
-  NODE_ENV !== 'test' && `${paths.dotenv}.local`,
-  paths.dotenv,
-].filter(Boolean)
+  NODE_ENV !== "test" && `${paths.dotenv}.local`,
+  paths.dotenv
+].filter(Boolean);
 
 // Load environment variables from .env* files. Suppress warnings using silent
 // if this file is missing. dotenv will never modify any environment variables
@@ -31,11 +31,11 @@ var dotenvFiles = [
 // https://github.com/motdotla/dotenv
 dotenvFiles.forEach(dotenvFile => {
   if (fs.existsSync(dotenvFile)) {
-    require('dotenv').config({
-      path: dotenvFile,
-    })
+    require("dotenv").config({
+      path: dotenvFile
+    });
   }
-})
+});
 
 // We support resolving modules according to `NODE_PATH`.
 // This lets you use absolute paths in imports inside large monorepos:
@@ -46,47 +46,50 @@ dotenvFiles.forEach(dotenvFile => {
 // Otherwise, we risk importing Node.js core modules into an app instead of Webpack shims.
 // https://github.com/facebookincubator/create-react-app/issues/1023#issuecomment-265344421
 // We also resolve them to make sure all tools using them work consistently.
-const appDirectory = fs.realpathSync(process.cwd())
-process.env.NODE_PATH = (process.env.NODE_PATH || '')
+const appDirectory = fs.realpathSync(process.cwd());
+process.env.NODE_PATH = (process.env.NODE_PATH || "")
   .split(path.delimiter)
   .filter(folder => folder && !path.isAbsolute(folder))
   .map(folder => path.resolve(appDirectory, folder))
-  .join(path.delimiter)
+  .join(path.delimiter);
 
 // Grab NODE_ENV and SDAPP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
-const SDAPP = /^SDAPP_/i
+const SDAPP = /^SDAPP_/i;
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
     .filter(key => SDAPP.test(key))
     .reduce(
       (env, key) => {
-        env[key] = process.env[key]
-        return env
+        env[key] = process.env[key];
+        return env;
       },
       {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
-        NODE_ENV: process.env.NODE_ENV || 'development',
+        NODE_ENV: process.env.NODE_ENV || "development",
         // Useful for resolving the correct path to static assets in `public`.
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
-        DEBUG_MODE: process.env.DEBUG_MODE || '',
-        DEV_BUILD: process.env.DEV_BUILD || process.env.NODE_ENV === 'development' ? 'true' : '',
+        DEBUG_MODE: process.env.DEBUG_MODE || "",
+        DEV_BUILD:
+          process.env.DEV_BUILD || process.env.NODE_ENV === "development"
+            ? "true"
+            : ""
       }
-    )
+    );
   // Stringify all values so we can feed into Webpack DefinePlugin
   const stringified = {
-    'process.env': Object.keys(raw).reduce((env, key) => {
-      env[key] = JSON.stringify(raw[key])
-      return env
-    }, {}),
-  }
+    "process.env": Object.keys(raw).reduce((env, key) => {
+      env[key] = JSON.stringify(raw[key]);
+      return env;
+    }, {})
+  };
 
-  return { raw, stringified }
+  return { raw, stringified };
 }
 
-module.exports = getClientEnvironment
+module.exports = getClientEnvironment;
